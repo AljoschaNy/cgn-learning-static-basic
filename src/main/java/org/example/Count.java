9bcde545e119aa9d74acc88cd513c629c642e1a1
Aljoscha Nyang <a.nyang@gmx.de>
2023-09-27 15:30:06 +0200

    Create several objects of Count and call instance and static methods

--- src/main/java/org/example/Count.java
+++ src/main/java/org/example/Count.java
@@ -6,10 +6,6 @@ public class Count {
     private static int totalCount = 0;
     private int instanceCount = 0;
 
-    public Count(int instanceCount) {
-        this.instanceCount = instanceCount;
-    }
-
     public static int getTotalCount() {
         return totalCount;
     }
